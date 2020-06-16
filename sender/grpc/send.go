5ebb44fb3b9e9c6e039aae55640cb32c05eac0cd
scottshotgg <nothing>
2020-06-16 12:38:53 -0500

    no errors now

--- sender/grpc/send.go
+++ sender/grpc/send.go
@@ -5,7 +5,7 @@ import (
 )
 
 func (g *grpcSender) Send(msg []byte) error {
-	g.sendClient.Send(&buffs.SendReq{
+	return g.sendClient.Send(&buffs.SendReq{
 		Msg: &buffs.Message{
 			Route:    g.route,
 			Contents: string(msg),
