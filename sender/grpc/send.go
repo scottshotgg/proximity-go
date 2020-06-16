217a03eb9cb30d74abe7d9f7c3820cbb9ca28e3c
scottshotgg <nothing>
2020-06-16 12:36:55 -0500

    maybe this is why?

--- sender/grpc/send.go
+++ sender/grpc/send.go
@@ -1,5 +1,9 @@
 package grpc
 
+import (
+	"github.com/scottshotgg/proximity/pkg/buffs"
+)
+
 func (g *grpcSender) Send(msg []byte) error {
 	g.sendClient.Send(&buffs.SendReq{
 		Msg: &buffs.Message{
